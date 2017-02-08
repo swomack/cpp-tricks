f200a83ac94df971dd1347b5723cb456dd9e0253
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-02-08 13:39:08 +0600

    * Move operator

--- Move and Copy/Move and Copy/Move and Copy/Source.cpp
+++ Move and Copy/Move and Copy/Move and Copy/Source.cpp
@@ -13,66 +13,44 @@ public:
 	A()
 	{
 		cout << "Constructor: A" << endl;
+		s = "Hello";
 	}
 
 	A(const A& ref)
 	{
 		cout << "Copy constructor: A" << endl;
+		s = ref.s;
 	}
+
+	A(A&& other)
+	{
+		cout << "Move constructor: A" << endl;
+		s = move(other.s);
+	}
+
+	string s;
 };
 
 
-class B
-{
-public:
-	B()
-	{
-		cout << "Constructor: B" << endl;
-	}
-
-	B(const B& ref)
-	{
-		cout << "Copy constructor: B" << endl;
-	}
-
-	A a;
-};
-
-class C
-{
-public:
-
-	C()
-	{
-		cout << "Constructor: C" << endl;
-	}
-
-
-	A a;
-};
-
-
-B get_b_object()
-{
-	B b;
-	return b;
-}
-
-C get_c_object()
-{
-	C c;
-	return c;
-}
 
 int main()
 {
-	// Copy constructor calls is recusrive from default copy constructor
+	A a;
 
-	// copy constructor and constructor behavior when return from function
-	B b = get_b_object();
+	// call the copy constructor
+	A aa = a;
 
-	// copy constructor and constructor behavior when return from function
-	C c = get_c_object();
+	cout << a.s << endl;
+	a.s[0] = 'L';
+	cout << aa.s << endl;
+
+	// call the move constructor if there is any
+	// if there is no move constructor, copy constructor will be called
+	A aaa = move(a);
+	cout << aaa.s << endl;
+	// after move, 'a' will be unusable(empty)
+	a.s[0] = 'H';
+	cout << a.s << endl;
 
 	getchar();
 	return 0;
