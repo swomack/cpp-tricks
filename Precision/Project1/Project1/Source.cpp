6c846d630b81d4ee1ca52cee392565a93be4dad9
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-04-30 13:54:48 +0600

    **Precision check**

---
+++ Precision/Project1/Project1/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < INT_MAX; i += 1000)
+	{
+		float k = (float)i;
+
+		if (i != (int)k)
+		{
+			int j = 0;
+			j++;
+		}
+	}
+	return 0;
+}
